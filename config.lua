a638eabe30c50027d3e3e2e4da2ba8d656afe7de
WilliamArnone <arnonewilliam@gmail.com>
2022-02-25 22:31:58 +0100

    libraries and bat

---
+++ config.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+    t.window.title = "GIOCO"
+    t.window.fullscreen = true
+    t.window.fullscreentype = "desktop"
+end
---
+++ main.lua
@@ -0,0 +1,43 @@
+local launch_type = arg[2]
+if launch_type == "test" or launch_type == "debug" then
+    require "lldebugger"
+
+    if launch_type == "debug" then
+        ---@diagnostic disable-next-line: undefined-global
+        lldebugger.start()
+    end
+end
+
+
+
+
+function love.load()
+    --love.window.setFullscreen( true )
+    lume = require "lume"
+    Object = require "classic" 
+end
+
+function love.update(dt)
+    
+end
+
+function love.draw()
+    love.graphics.print("Hello Word")
+end
+
+
+
+
+---@diagnostic disable-next-line: undefined-field
+local love_errorhandler = love.errhand
+
+function love.errorhandler(msg)
+    ---@diagnostic disable-next-line: undefined-global
+    if lldebugger then
+        ---@diagnostic disable-next-line: undefined-global
+        lldebugger.start() -- Add this
+        error(msg, 2)
+    else
+        return love_errorhandler(msg)
+    end
+end
