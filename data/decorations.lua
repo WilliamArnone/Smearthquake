43b1ede2ea71f31960bfebcf975e626bdb9c6a00
WilliamArnone <arnonewilliam@gmail.com>
2022-03-04 20:27:23 +0100

    patch bilanciamento

--- data/decorations.lua
+++ data/decorations.lua
@@ -5,26 +5,26 @@ decoration_ratio = {}
 function initDecorations()
     decorations = {}
     --                                          width, height, price, happiness, instability, img, sound
-    decorations["vase"] = function () return Decoration(18, 18, 10, 10, 10, 1, sounds.glass) end
-    decorations["globe"] = function () return Decoration(24, 32, 15, 20, 15, 13, sounds.hard) end
+    decorations["vase"] = function () return Decoration(18, 18, 10, 15, 10, 1, sounds.glass) end
+    decorations["globe"] = function () return Decoration(24, 32, 15, 30, 15, 13, sounds.hard) end
     
-    decorations["vault"] = function () return Decoration(18, 23, 20, 25, 15, 6, sounds.hard) end
-    decorations["waterman"] = function () return Decoration(18, 23, 25, 30, 15, 7, sounds.hard) end
-    decorations["cubeportal"] = function () return Decoration(27, 28, 25, 20, 25, 2, sounds.hard) end
+    decorations["vault"] = function () return Decoration(18, 23, 20, 35, 15, 6, sounds.hard) end
+    decorations["waterman"] = function () return Decoration(18, 23, 25, 40, 15, 7, sounds.hard) end
+    decorations["cubeportal"] = function () return Decoration(27, 28, 25, 30, 25, 2, sounds.hard) end
     
-    decorations["amongus"] = function () return Decoration(17, 27, 50, 70, 15, 15, sounds.glass) end
-    decorations["tetris"] = function () return Decoration(29, 21, 60, 50, 30, 14, sounds.glass) end
+    decorations["amongus"] = function () return Decoration(17, 27, 50, 90, 15, 15, sounds.glass) end
+    decorations["tetris"] = function () return Decoration(29, 21, 60, 60, 30, 14, sounds.glass) end
     
-    decorations["mario"] = function () return Decoration(16, 16, 90, 80, 40, 8, sounds.hard) end
+    decorations["mario"] = function () return Decoration(16, 16, 90, 100, 40, 8, sounds.hard) end
     
-    decorations["pop"] = function () return Decoration(28, 32, 200, 250, 10, 5, sounds.hard) end
-    decorations["venusaur"] = function () return Decoration(24, 21, 200, 15, 5, 11, sounds.hard) end
+    decorations["pop"] = function () return Decoration(28, 32, 200, 320, 10, 5, sounds.hard) end
+    decorations["venusaur"] = function () return Decoration(24, 21, 200, 200, 5, 11, sounds.hard) end
     
     
-    decorations["triforce"] = function () return Decoration(18, 10, 300, 400, 30, 10, sounds.hard) end
-    decorations["layton"] = function () return Decoration(16, 13, 500, 560, 20, 9, sounds.hard) end
-    decorations["sparasemi"] = function () return Decoration(22, 28, 550, 600, 10, 3, sounds.glass) end
-    decorations["ds"] = function () return Decoration(22, 27, 600, 900, 5, 12, sounds.glass) end
+    decorations["triforce"] = function () return Decoration(18, 10, 300, 550, 30, 10, sounds.hard) end
+    decorations["layton"] = function () return Decoration(16, 13, 500, 700, 20, 9, sounds.hard) end
+    decorations["sparasemi"] = function () return Decoration(22, 28, 550, 800, 10, 3, sounds.glass) end
+    decorations["ds"] = function () return Decoration(22, 27, 600, 1200, 10, 12, sounds.glass) end
     
     decorations["rubik"] = function () return Decoration(13, 12, 1, 5000, 5, 4, sounds.hard) end
 
--- data/posters.lua
+++ data/posters.lua
@@ -5,17 +5,17 @@ posters_ratio = {}
 function initPosters()
     posters = {}
     --                                          width, height, price, happiness, instability, img, sound
-    posters["monnalisa"] = function () return Poster(26, 32, 1000, 300, 40, 1, sounds.hard) end      --0.3
-    posters["specchiodellebrame"] = function () return Poster(17, 32, 500, 170, 30, 2, sounds.glass) end  --0.34
-    posters["munch"] = function () return Poster(30, 32, 750, 240, 35, 3, sounds.hard) end      --0.32
+    posters["monnalisa"] = function () return Poster(26, 32, 1000, 400, 40, 1, sounds.hard) end      --0.3
+    posters["specchiodellebrame"] = function () return Poster(17, 32, 500, 240, 30, 2, sounds.glass) end  --0.34
+    posters["munch"] = function () return Poster(30, 32, 700, 320, 35, 3, sounds.hard) end      --0.32
 
-    posters["naruto"] = function () return Poster(32, 32, 30, 30, 15, 5, sounds.hard) end  --1
-    posters["dnd"] = function () return Poster(25, 32, 30, 15, 30, 8, sounds.hard) end  --0.5
+    posters["naruto"] = function () return Poster(32, 32, 30, 40, 15, 5, sounds.hard) end  --1
+    posters["dnd"] = function () return Poster(25, 32, 30, 20, 30, 8, sounds.hard) end  --0.5
 
-    posters["madmax"] = function () return Poster(32, 28, 60, 30, 50, 4, sounds.hard) end   --0.5
-    posters["sonic"] = function () return Poster(32, 32, 60, 50, 30, 6, sounds.hard) end  --~1
+    posters["madmax"] = function () return Poster(32, 28, 60, 40, 50, 4, sounds.hard) end   --0.5
+    posters["sonic"] = function () return Poster(32, 32, 60, 75, 30, 6, sounds.hard) end  --~1
 
-    posters["scott"] = function () return Poster(28, 32, 80, 20, 200, 9, sounds.hard) end  --0.25
+    posters["scott"] = function () return Poster(28, 32, 80, 20, 300, 9, sounds.hard) end  --0.25
 
     local xspace, yspace, pricespace, happyspace, instabspace = 15, 13, 15, 5, 10            --0.3
     posters["yellowspace"] = function () return Poster(xspace, yspace, pricespace, happyspace, instabspace, 7, sounds.glass) end
